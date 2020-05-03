db7c6b5d82a1c223bd658c3bbad6d33e2345a2fa
Lorenzo Setale <lorenzo@setale.me>
2020-05-03 22:51:04 +0200

    adds a "view existing widget"

--- src/setup.jsx
+++ src/setup.jsx
@@ -16,6 +16,7 @@ class SetupKey extends Component {
     this.state = { 
       domain: "", 
       key: "",
+      tempKey: "", // used to fill before rendering an existing one
       resetDB: false,
     };
 
@@ -27,7 +28,7 @@ class SetupKey extends Component {
       })
   }
 
-  updateState(e) {
+  updateDomainState(e) {
     this.setState({domain: e.target.value})
   }
 
@@ -42,6 +43,14 @@ class SetupKey extends Component {
       })
   }
 
+  updateDBKeyValue(e) {
+    this.setState({tempKey: e.target.value})
+  }
+
+  showExistingDB(e) {
+    this.setState({key: this.state.tempKey})
+  }
+
   toggleFoceNew(e) {
     e.preventDefault()
 
@@ -51,26 +60,44 @@ class SetupKey extends Component {
   render() {
     if(!this.state.loaded) return <div>Loading... Please wait</div>
     return (
-      <div>  
-        <label for="domainTextbox">Your domain:</label>
-        <input 
-          type="text" 
-          id="domainTextbox"
-          value={this.state.domain}
-          onChange={this.updateState.bind(this)} 
-        />
-        <button onClick={this.generateAddress.bind(this)}> Generate Key </button>
-        <input onChange={this.toggleFoceNew.bind(this)} type="checkbox" id="resetCheck"></input>
-        <label for="resetCheck">Reset DB</label>
+      <div>
+        <div>
+          <h3>New Widget</h3>
+          <label for="domainTextbox">Your domain:</label>
+          <input 
+            type="text" 
+            id="domainTextbox"
+            value={this.state.domain}
+            onChange={this.updateDomainState.bind(this)} 
+          />
+          <button onClick={this.generateAddress.bind(this)}> Generate Key </button>
+          <input onChange={this.toggleFoceNew.bind(this)} type="checkbox" id="resetCheck"></input>
+          <label for="resetCheck">Reset DB</label>
+        </div>
+        <div>
+          <h3>Existing Widget</h3>
+          <label for="dbkey">Your dbkey:</label>
+          <input 
+            type="text" 
+            id="dbkey"
+            value={this.state.tempKey}
+            onChange={this.updateDBKeyValue.bind(this)} 
+          />
+          <button onClick={this.showExistingDB.bind(this)}> Show button </button>
+        </div>
         {!!this.state.key && 
-          <div>
-            Your Code: 
-            <p><code>{`<div data-prop-dbkey="${this.state.key}"><script src="dlike.js"></script></div>`}</code></p>
-            DO NOT close this window before visiting your website!<br />
-            This is your button: <br />
-            <LikeCounter dbkey={this.state.key} />
-          </div>
-        }
+            <div>
+              <h3>Results</h3>
+              Widget Code: 
+              <p><code>{`<div data-prop-dbkey="${this.state.key}"><script src="dlike.js"></script></div>`}</code></p>
+              This is your button running live: <br />
+              <br />
+              <LikeCounter dbkey={this.state.key} /><br />
+              <br />
+              DO NOT close this window before visiting your website!
+            </div>
+          }
+
       </div>
     )
   }
